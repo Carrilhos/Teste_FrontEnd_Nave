47c335a9ac16fb01afe8fdae613576e571994869
Carrilhos <gcarrilhos@gmail.com>
2020-07-09 03:13:28 -0300

    adicionado modal

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import { IoIosArrowBack } from "react-icons/io";
+import { Modal } from '@material-ui/core/'
+import { makeStyles } from '@material-ui/core/styles'
 
 import './index.css'
 import NavBar from '../../componets/navBar'
 
-export default function register(){
+function rand() {
+    return Math.round(Math.random() * 20) - 10;
+  }
+
+function getModalStyle() {
+    const top = 50 
+    const left = 50
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+      };
+    }
+
+export default function Register(){
+    const useStyles = makeStyles((theme) => ({
+        paper: {
+          position: 'absolute',
+          width: 400,
+          backgroundColor: theme.palette.background.paper,
+          boxShadow: theme.shadows[5],
+          padding: theme.spacing(2, 4, 3),
+        },
+      }));
+
+
+    const classes = useStyles();
+    const [modalStyle] = React.useState(getModalStyle);
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+      };
+    
+      const handleClose = () => {
+        setOpen(false);
+      };
+
+
     return(
+        
     <div>
         <NavBar/>
         <div className="centralizar">
@@ -60,11 +102,28 @@ export default function register(){
                                 type="email"
                             />
                             <div className="buttonPosition">
-                                <Link to="/home">
-                                    <button className="buttonStyle">
+                                <Link>
+                                    <button className="buttonStyle" onClick={handleOpen}>
                                         Salvar
                                     </button>
                                 </Link>
+                    
+                                <Modal
+                                open={open}
+                                onClose={handleClose}
+                                aria-labelledby="simple-modal-title"
+                                aria-describedby="simple-modal-description"
+                            >
+                                {
+                                    <div style={modalStyle} className={classes.paper}>
+                                    <h2 id="simple-modal-title">Naver criado</h2>
+                                    <p id="simple-modal-description">
+                                        Naver criado com sucesso!
+                                    </p>
+                                    
+                                  </div>
+                                }
+                            </Modal>
                             </div> 
                         </div>
                     </form>
