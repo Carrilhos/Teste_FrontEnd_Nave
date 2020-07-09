f9a5ddaea5301a28a361c8bfb2f752a3f336a6f9
Carrilhos <gcarrilhos@gmail.com>
2020-07-09 03:21:14 -0300

    Identação

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -25,11 +25,11 @@ function getModalStyle() {
 export default function Register(){
     const useStyles = makeStyles((theme) => ({
         paper: {
-          position: 'absolute',
-          width: 400,
-          backgroundColor: theme.palette.background.paper,
-          boxShadow: theme.shadows[5],
-          padding: theme.spacing(2, 4, 3),
+            position: 'absolute',
+            width: 400,
+            backgroundColor: theme.palette.background.paper,
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3),
         },
       }));
 
